5e5839805b7d7c669e5eb3923e5f475e54b71b42
Leo Oliveira <qwerty124563@gmail.com>
2021-03-09 12:29:35 +0000

    Implement a Reader Struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,55 @@
 use clap::{Arg, App, SubCommand};
 
-use linefeed::{Interface, ReadResult};
+use linefeed::{Interface, ReadResult, DefaultTerminal};
 use std::borrow::Borrow;
 use std::ptr::read;
+
 const FILE_HISTORY: &str = ".history";
+const PROMPT_MESSAGE: &str = "ldb >>> ";
+
+
+struct Reader {
+    interface: Interface<DefaultTerminal>,
+
+}
+
+impl Reader {
+    fn new() -> Self {
+        let mut reader = Interface::new("ldb")
+            .expect("Can't create a new application.");
+
+        // Set prompt
+        reader.set_prompt(PROMPT_MESSAGE)
+            .expect("Can't set a prompt.");
+
+        // Load history
+        if reader.load_history(FILE_HISTORY).is_err() {
+            println!("No History.");
+        }
+
+        Self {
+            interface: reader,
+        }
+    }
+
+    fn save_history(&self, line: String) {
+        if !line.trim().is_empty() {
+            self.get_interface()
+                .add_history_unique(line.clone());
+        }
+    }
+
+    fn get_interface(&self) -> &Interface<DefaultTerminal> {
+        &self.interface
+    }
+
+    fn read_line(&self) -> ReadResult {
+        self.get_interface()
+            .read_line()
+            .expect("Cant read line.")
+    }
+}
+
 
 fn main() {
     let matches = App::new("ldb")
@@ -12,33 +58,15 @@ fn main() {
         .about("A Database developed using Rust.")
         .get_matches();
 
-    let mut reader = Interface::new("ldb")
-        .expect("Can't create a new application.");
+    let reader = Reader::new();
 
-    reader.set_prompt("ldb >>> ")
-        .expect("Can't set a prompt.");
-
-    // Load history
-    if reader.load_history(FILE_HISTORY).is_err() {
-        println!("No History.");
+    while let ReadResult::Input(input) = reader.read_line() {
+        println!("got input {:?}", input);
     }
-
-    loop {
-        let line = reader
-            .read_line()
-            .expect("Cant read line");
-
-        match line {
-            ReadResult::Input(input) => {
-                // Parse result
-                println!("got input {:?}", input);
-            }
-            _ => panic!("Error getting the input."),
-        }
-    }
-
 
     println!("Goodbye.");
 
     println!("Hello, world!");
 }
+
+fn save_history() {}
